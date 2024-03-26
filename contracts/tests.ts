c36d97b82a8414812f02f638cafe0dbeb51e0ccd
jeffersonmatias <138624962+jeffersonmatias@users.noreply.github.com>
2024-03-26 15:58:14 -0300

    deleted archives

--- contracts/tests.ts
+++ contracts/tests.ts
@@ -4,15 +4,3 @@
  * Feel free to let us know via PR, if you find something broken in this contract
  * file.
  */
-
-import '@japa/runner'
-
-declare module '@japa/runner' {
-  interface TestContext {
-    // Extend context
-  }
-
-  interface Test<TestData> {
-    // Extend test
-  }
-}
